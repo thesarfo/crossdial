37674357106895f7db6f23fb289fc7bca296466c
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-08-07 18:12:44 +0000

    throw exception on wrong field annotation

--- src/main/java/dev/thesarfo/PhoneNumberProcessor.java
+++ src/main/java/dev/thesarfo/PhoneNumberProcessor.java
@@ -1,8 +1,9 @@
 package dev.thesarfo;
 
+import com.google.auto.service.AutoService;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
@@ -11,9 +12,14 @@ import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
 import java.util.Set;
 
-@SupportedAnnotationTypes("dev.thesarfo.PhoneNumber")
+@AutoService(Processor.class)
 @SupportedSourceVersion(SourceVersion.RELEASE_17)
 public class PhoneNumberProcessor extends AbstractProcessor {
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return Set.of(PhoneNumber.class.getCanonicalName());
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
@@ -25,7 +31,7 @@ public class PhoneNumberProcessor extends AbstractProcessor {
             }
             if (!element.asType().toString().equals("java.lang.String")) {
                 processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
-                        "@PhoneNumber can only be applied to Strings.", element);
+                        "@PhoneNumber can only be applied to String fields.", element);
             }
         }
         return true;
--- src/main/java/dev/thesarfo/User.java
+++ src/main/java/dev/thesarfo/User.java
@@ -6,9 +6,14 @@ public class User {
     @PhoneNumber
     private String phoneNumber;
 
+    @PhoneNumber
+    private int testing;
+
     public User(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+
 
     public String getPhoneNumber() {
         return phoneNumber;
