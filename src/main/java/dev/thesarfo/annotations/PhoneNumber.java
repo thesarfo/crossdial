0421e64587cd7e03706a268b11f5eb342ecad970
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-01-02 00:54:41 +0000

    add phonenumber annotation

--- src/main/java/dev/thesarfo/annotations/PhoneNumber.java
+++ src/main/java/dev/thesarfo/annotations/PhoneNumber.java
@@ -1,11 +1,15 @@
-package dev.thesarfo;
+package dev.thesarfo.annotations;
+
+import dev.thesarfo.validators.PhoneNumberInspector;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
+@Inspect(inspector = PhoneNumberInspector.class, message = "Invalid phone number.")
 public @interface PhoneNumber {
+    String message() default "Invalid phone number.";
 }
